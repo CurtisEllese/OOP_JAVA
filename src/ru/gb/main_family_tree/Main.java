88c52259bb224ae203a30864092d55e78adc7239
CurtisEllese <mama-karina@bk.ru>
2023-08-25 23:03:51 +0600

    Added Second Family Tree. Used service

--- src/ru/gb/main_family_tree/Main.java
+++ src/ru/gb/main_family_tree/Main.java
@@ -8,23 +8,33 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        FamilyTree tree = new FamilyTree();
+        Service service = new Service();
 
-        tree.add(new Human("Frederick", Gender.Male, LocalDate.of(1905, 10, 11),
-                LocalDate.of(1999, 6, 25)));
-        tree.add(new Human("Mary", Gender.Female, LocalDate.of(1912, 5, 10),
-                LocalDate.of(2000, 8, 7)));
-        tree.add(new Human("Donald", Gender.Male, LocalDate.of(1946, 6, 14),
-                tree.getByName("Frederick"), tree.getByName("Mary")));
-        tree.add(new Human("Ivana", Gender.Female, LocalDate.of(1949, 2, 20),
-                LocalDate.of(2022, 7, 14)));
-        tree.add(new Human("Donald Jr", Gender.Male, LocalDate.of(1977, 12, 31),
-                tree.getByName("Donald"), tree.getByName("Ivana")));
-        tree.add(new Human("Ivanka", Gender.Female, LocalDate.of(1981, 10, 30),
-                tree.getByName("Donald"), tree.getByName("Ivana")));
-        tree.add(new Human("Eric", Gender.Male, LocalDate.of(1984, 1, 6),
-                tree.getByName("Donald"), tree.getByName("Ivana")));
+        service.createEmptyFamilyTree();
+        service.addPerson(new Human("Frederick", Gender.Male, LocalDate.of(1905, 10, 11),
+                LocalDate.of(1999, 6, 25)), 0);
+        service.addPerson(new Human("Mary", Gender.Female, LocalDate.of(1912, 5, 10),
+                LocalDate.of(2000, 8, 7)), 0);
+        service.addPerson(new Human("Donald", Gender.Male, LocalDate.of(1946, 6, 14),
+                service.getByName("Frederick", 0), service.getByName("Mary", 0)), 0);
+        service.addPerson(new Human("Ivana", Gender.Female, LocalDate.of(1949, 2, 20),
+                LocalDate.of(2022, 7, 14)), 0);
+        service.addPerson(new Human("Donald Jr", Gender.Male, LocalDate.of(1977, 12, 31),
+                service.getByName("Donald", 0), service.getByName("Ivana", 0)), 0);
+        service.addPerson(new Human("Ivanka", Gender.Female, LocalDate.of(1981, 10, 30),
+                service.getByName("Donald", 0), service.getByName("Ivana", 0)), 0);
+        service.addPerson(new Human("Eric", Gender.Male, LocalDate.of(1984, 1, 6),
+                service.getByName("Donald", 0), service.getByName("Ivana", 0)), 0);
 
-        System.out.println(tree.getInfo());
+
+        service.createEmptyFamilyTree();
+        service.addPerson(new Human("Ivan", Gender.Male, LocalDate.of(1990, 10, 11)), 1);
+        service.addPerson(new Human("Anastasia", Gender.Female, LocalDate.of(1985, 5, 10)), 1);
+        service.addPerson(new Human("Maria", Gender.Female, LocalDate.of(2010, 6, 14),
+                service.getByName("Ivan", 1), service.getByName("Anastasia", 1)), 1);
+
+
+
+        System.out.println(service.getInfo());
     }
 }
